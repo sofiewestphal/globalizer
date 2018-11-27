e01fe77d0df0a64a14fea5bc0eeefc30f54d5075
saraaaroeejepsen <sara@festina.xxx>
2018-11-27 15:05:00 +0100

    added search function

--- src/components/SearchBar/SearchField.js
+++ src/components/SearchBar/SearchField.js
@@ -1,28 +1,30 @@
 import React from 'react';
 import './index.scss';
 import PropTypes from 'prop-types';
-// import { Search } from '../../assets/icons/icons';
+import { Search } from '../../assets/icons/icons';
 
 
 const SearchField = (props) => {
-  const { onSearch } = props;
+  const { handleSearch, value } = props;
   return (
-    <div id="input_container">
+    <div className="input_container">
       <input
         id="searchInput"
         placeholder="Search activities..."
-        onChangeText={onSearch}
+        value={value}
+        onChange={event => handleSearch(event.target.value)}
       />
-      {/* <Search
-        width="20px"
-        height="20px"
-      /> */}
+      <Search
+        width="15px"
+        height="15px"
+      />
     </div>
   );
 };
 
 SearchField.propTypes = {
-  onSearch: PropTypes.func.isRequired,
+  handleSearch: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired,
 };
 
 export default SearchField;
