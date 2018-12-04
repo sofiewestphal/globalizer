3b7db045dd09f51e72d5107e2d19e05ecf1de5f6
Sofie Westphal <sofiewestphal@gmail.com>
2018-12-04 17:34:11 +0000

    moved activities from store to db

--- frontend/src/reducers/usersReducer.js
+++ frontend/src/reducers/usersReducer.js
@@ -1,35 +1,7 @@
 import { RESET, USER_ADD } from '../actions/actionCreators';
 
 const initialState = {
-  users: [
-    {
-      id: 1,
-      name: 'Sara',
-      lastname: 'Jepsen',
-      age: 25,
-      email: 's',
-      password: '1',
-      categories: ['Food & Drinks', 'Music', 'Sports', 'Beauty & Wellness', 'Outdoor', 'Culture', 'Hobbies & Crafts'],
-    },
-    {
-      id: 2,
-      name: 'Sofie',
-      lastname: 'Pedersen',
-      age: 27,
-      email: 'sofiewestphal@gmail.com',
-      password: '123456',
-      categories: ['Beauty & Wellness', 'Outdoor', 'Sports'],
-    },
-    {
-      userId: 3,
-      name: 'Gavin',
-      lastname: 'Lerner',
-      age: 27,
-      email: 'gavinlerner@hotmail.com',
-      password: '12345678',
-      categories: ['Culture', 'Hobbies & Crafts', 'Outdoor', 'Sports'],
-    },
-  ],
+  users: [],
 };
 
 export default function usersReducer(state = initialState, action) {
